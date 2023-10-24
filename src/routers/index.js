bf298d39ce6aedd0e5e7da4ccc9148c195e3b61d
Harika.padamata <harika.actimize@gmail.com>
2023-10-24 15:14:56 +0530

    db changes

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=> {
-    res.json({status: 200, message: "server is running"})
+    res.status(200).json({status: 200, message: "server is running"})
 })
 
 module.exports = router
